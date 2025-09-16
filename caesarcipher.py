63e452e9dc1279e4490cf3b9540082147f202b89
Bruce Le <broocethegoose@Bruces-Macbook-Air.local>
2025-09-15 20:44:41 -0700

    Added the Encyrpt and Decrypt functions.

--- caesarcipher.py
+++ caesarcipher.py
@@ -3,16 +3,28 @@ from tkinter import messagebox
 
 
 
-
-def encrypt(plaintext: str, key: int) -> str:
+"""The function takes in a string as an argument and loops over each character 
+to shift it by the key and return the characters back as a list.
+"""
+def encrypt(plaintext: str, key: int) -> list:
     key %= 26
+    enc_message = []
     for ch in plaintext:
-        
-    ciphertext = plaintext + key
+        enc_message.append(chr(ord(ch) + key))
+    print(enc_message)
+    
+    return enc_message
 
-
-#def decrypt(ciphertext, key):
-    #plaintext = ciphertext - key
+"""This function decrypts the message when provided a ciphertext and key by subtracting
+the key to return the message to the original state"""
+def decrypt(ciphertext: str, key: int)-> str: 
+    dec_message = []
+    for ch in ciphertext:
+        dec_message.append(chr(ord(ch) - key))
+    print(dec_message)
+    
+    return dec_message
+    
 
 input_key = int(input("Enter a key (0-25): "))
 user_input = str(input("Enter a message to encrypt: ")).strip()
